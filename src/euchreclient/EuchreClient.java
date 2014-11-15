0408df723389816a35ecfe5851cb40260e227b49
Benjamin Nold <brnold@oakland.edu>
2014-11-14 19:56:00 -0500

    Added Object pusher code

--- src/euchreclient/EuchreClient.java
+++ src/euchreclient/EuchreClient.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -7,8 +7,13 @@
 package euchreclient;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
@@ -30,10 +35,25 @@ public class EuchreClient
     boolean openPort(String ip, int port)
     {
         try {
-            Socket kkSocket = new Socket(ip, port);
-            PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(
-            new InputStreamReader(kkSocket.getInputStream()));
+           // ServerSocket ss = new ServerSocket( port);
+            Socket s = new Socket(ip, port);
+            
+            ObjectOutputStream objectOut = new ObjectOutputStream(s.getOutputStream());
+            objectOut.flush();
+            //InputStream input = s.getInputStream();
+      ObjectInputStream objectin = new ObjectInputStream(s.getInputStream());
+      
+      //OutputStream output = s.getOutputStream();
+     
+            String sss = "sending from da client";
+            objectOut.writeObject(sss);
+            objectOut.flush();
+            String asdf = (String)objectin.readObject();
+            System.out.println(asdf);
+            
+            asdf = (String)objectin.readObject();
+            System.out.println(asdf);
+            
         }catch(Exception e){
             System.out.println(e);
             return false;
@@ -41,8 +61,5 @@ public class EuchreClient
         return true;
     }
     
-    public boolean sendName(String name)
-    {
-        
-    }
+  
 }
