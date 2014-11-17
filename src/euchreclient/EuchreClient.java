1b4e4db19647a624ab97e190e1ea519b420269f1
Benjamin Nold <brnold@oakland.edu>
2014-11-16 19:15:17 -0500

    Added more sever logic. Method to get objects from the Socket

--- src/euchreclient/EuchreClient.java
+++ src/euchreclient/EuchreClient.java
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package euchreclient;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
@@ -22,7 +22,14 @@ import java.net.Socket;
  */
 public class EuchreClient
 {
+
     String name;
+    Object o;
+    ObjectInputStream objectin;
+    ObjectOutputStream objectOut;
+    Socket s;
+    
+
     /**
      * @param args the command line arguments
      */
@@ -31,35 +38,50 @@ public class EuchreClient
         name = "fred";
         System.out.println("Object created");
     }
-    
+
     boolean openPort(String ip, int port)
     {
-        try {
-           // ServerSocket ss = new ServerSocket( port);
-            Socket s = new Socket(ip, port);
-            
-            ObjectOutputStream objectOut = new ObjectOutputStream(s.getOutputStream());
+        try
+        {
+            // ServerSocket ss = new ServerSocket( port);
+            s = new Socket(ip, port);
+
+            objectOut = new ObjectOutputStream(s.getOutputStream());
             objectOut.flush();
             //InputStream input = s.getInputStream();
-      ObjectInputStream objectin = new ObjectInputStream(s.getInputStream());
-      
+            objectin = new ObjectInputStream(s.getInputStream());
+            
+            
+
       //OutputStream output = s.getOutputStream();
-     
-            String sss = "sending from da client";
-            objectOut.writeObject(sss);
-            objectOut.flush();
-            String asdf = (String)objectin.readObject();
-            System.out.println(asdf);
+            /*
+             String sss = "sending from da client";
+             objectOut.writeObject(sss);
+             objectOut.flush();
+             String asdf = (String)objectin.readObject();
+             System.out.println(asdf);
             
-            asdf = (String)objectin.readObject();
-            System.out.println(asdf);
-            
-        }catch(Exception e){
+             asdf = (String)objectin.readObject();
+             System.out.println(asdf);
+             */
+        } catch (Exception e)
+        {
             System.out.println(e);
             return false;
-         }
+        }
         return true;
     }
     
-  
+    public void workSocket()
+    {
+        try{
+        if(objectin.available() > 0)
+                 o = objectin.readObject();
+        }catch(IOException e){
+            System.out.println(e);
+        }catch(ClassNotFoundException e){
+            System.out.println(e);
+        }
+    }
+
 }
